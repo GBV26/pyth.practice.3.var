a0ac328c2acb399688747b6d2dc4238daf7f03f7
GBV26 <gavrilin.nekita@gmail.com>
2025-01-29 13:21:09 +0300

    3 практическая.py

--- 3 практическая.py
+++ 3 практическая.py
@@ -2,14 +2,11 @@
 def calculate_time(minutes_passed):
   if minutes_passed < 0:
       return 0, 0
-    
   total_minutes_in_day = 24 * 60 
   minutes_passed = minutes_passed % total_minutes_in_day
-
   hours = minutes_passed // 60
   minutes = minutes_passed % 60
   return hours, minutes
-
 # Пример использования:
 n = int(input("Введите количество минут, прошедших с начала суток: "))
 hours, minutes = calculate_time(n)
